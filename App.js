b7d2ccb4c417b522117e3a14952da59f83c38f2d
Benedict <newlyf38@gmail.com>
2023-01-07 08:50:33 +0000

    done with the header of app

--- App.js
+++ App.js
@@ -1,14 +1,17 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import React from 'react';
+import { Ionicons } from '@expo/vector-icons';
 
 
 export default function App() {
   const COLOR = {primary: '#1f145c', while: '#fff'}
   return (
-    <View style={styles.container}>
-      <Text>Welcome to today todo!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+          <Text style={{fontWeight:'bold', color:'red'}}>TODAY TODO</Text>
+          <Ionicons name='trash' style={styles.trashIcon} />
+      </View>
+    </SafeAreaView>
   );
 }
 
@@ -16,7 +19,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  header:{
+    padding:20,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  }, 
+  trashIcon:{
+    fontSize:20,
+    color:'red',
+    fontWeight: 'bold'
+  }
 });
