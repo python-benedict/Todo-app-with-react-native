c5e20f556f892613843380dbf14e2b5a959d2e97
Benedict <newlyf38@gmail.com>
2023-01-07 08:24:58 +0000

    added a primary color in a form of objects which i will be using for the rest of the project

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
 
 export default function App() {
+  const COLOR = {primary: '#1f145c', while: '#fff'}
   return (
     <View style={styles.container}>
       <Text>Welcome to today todo!</Text>
